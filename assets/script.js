dff1be9eeec26b447eb90faeeafc565f25aadbde
PengasKhan <cheesepuff42@gmail.com>
2023-07-26 18:36:38 -0500

    Airport API functioning
    functioning API-Ninjas fetch, not connected to selector yet

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,80 @@
-var selectEl = document.querySelector('#states');
+var selectEl = document.querySelector("#states");
+const airportRequest =
+  "https://api.api-ninjas.com/v1/airports?region=California";
+const airportOptions = {
+  headers: {
+    "Content-Type": "application/json",
+    "X-Api-Key": "bnQwuT6BX0QsPXbYebfY/A==Y1ZwZQ6OLOMQOTSp",
+  },
+};
 
-
-var statesList = ['Select','Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
-
-for (let i = 0; i < statesList.length; i++){
-    var optionEl = document.createElement('option');
-    optionEl.textContent = statesList[i];
-    optionEl.setAttribute("value", statesList[i]);
-    optionEl.setAttribute("class", "option-element");
-    //console.log(optionEl);
-    selectEl.appendChild(optionEl);
-}
+var statesList = [
+  "Select",
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming",
+];
 
 // var x = document.querySelector("option[value=Select]");
 // x.setAttribute("disable", "true")
 // console.log("X: ", x)
 
-fetch('https://api.publicapis.org/entries')
-.then(function(response) {
-    console.log("RES: ", response)
-    return response.json();
-})
-.then(function (data) {
-    console.log("DATA: ", data.entries[0].Description)
-    var x = document.createElement("p");
-    x.textContent = data.entries[0].Description;
-    document.querySelector("body").appendChild(x)
-})
-.catch(function (err) {
-    console.log(err)
-})
+function getApi(airportRequestUrl) {
+  fetch(airportRequestUrl, airportOptions)
+    .then(function (response) {
+      console.log(response.status);
+      return response.json();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
+getApi(airportRequest);
